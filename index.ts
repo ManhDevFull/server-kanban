3f027548c79d9598f4e9ec2de7d3997893ede761
ManhDevFull <thanhmanhdangfa@gmail.com>
2024-10-24 14:15:43 +0700

    run server

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { error } from 'console'
+import userRouter from "./src/routers/user"
 dotenv.config()
 
-const app =  express()
-
-app.listen(process.env.PORT, (err?: any) => {
-    if(err){
-        throw new Error(err)
+const app = express()
+app.use('/auth', userRouter)
+const connectDB = async () => {
+    try {
+        console.log(`Connect to db successfully!!!`)
+    } catch (error) {
+        console.log(`Can not  connect to db ${error}`)
     }
-    console.log(`Server is starting at http://localhost:${process.env.PORT}`)
-});
+}
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, (err?: any) => {
+        if (err) {
+            throw new Error(err)
+        }
+        console.log(`Server is starting at http://localhost:${process.env.PORT}`)
+    })
+}).catch((error) => {
+    console.log(error)
+})
---
+++ src/models/UserModel.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now(),
+    }
+})
+const UserModel = mongoose.model('users', UserSchema)
+export default UserModel
